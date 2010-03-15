da0237e96507e076de734cef5ef264b147a855e7
peter.hogh.mikkelsen <peter.hogh.mikkelsen@49e60964-1571-11df-9d2a-2365f6df44e6>
2010-03-15 11:32:52 +0000

    OS API added. /rpyLocal added for local changes to /rpy

---
+++ exercise4/trunk/source/embsysx/src/os/posix/BinarySemaphore.cpp
@@ -0,0 +1,18 @@
+/*
+ * BinSem.cpp
+ *
+ *  Created on: Mar 11, 2010
+ *      Author: stud
+ */
+
+#include "BinarySemaphore.h"
+#if 0
+BinarySemaphore::BinarySemaphore() {
+	// TODO Auto-generated constructor stub
+
+}
+
+BinarySemaphore::~BinarySemaphore() {
+	// TODO Auto-generated destructor stub
+}
+#endif
