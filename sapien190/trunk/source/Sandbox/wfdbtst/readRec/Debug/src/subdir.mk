3c31b30624e4a975a219a785b7e0510f79ac7c36
peter.hogh.mikkelsen <peter.hogh.mikkelsen@49e60964-1571-11df-9d2a-2365f6df44e6>
2010-05-08 21:55:18 +0000

    Annotations added...

--- sapien190/trunk/source/Sandbox/wfdbtst/readRec/Debug/src/subdir.mk
+++ sapien190/trunk/source/Sandbox/wfdbtst/readRec/Debug/src/subdir.mk
@@ -17,7 +17,7 @@ CPP_DEPS += \
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	g++ -I/home/stud/work/courses/tiirts/sapien190/trunk/source/Lib -I/home/stud/work/courses/tiirts/sapien190/trunk/source/Lib/lib-arm/wfdb-arm -I/home/stud/work/courses/tiirts/sapien190/trunk/source/Lib/lib-arm -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
--- sapien190/trunk/source/Sandbox/wfdbtst/readRec/src/readRec.cpp
+++ sapien190/trunk/source/Sandbox/wfdbtst/readRec/src/readRec.cpp
@@ -10,12 +10,22 @@
 using namespace std;
 #include <stdio.h>
 #include <wfdb/wfdb.h>
+#include <wfdb/ecgcodes.h>
+#define SAPIEN_SAMPLING_FREQ 250
+
 
 int main(int argc, char *argv[])
 {
+	int i, nsig;
 	char record[100];
 	WFDB_Sample v[2];
-	WFDB_Siginfo s[2];
+	WFDB_Anninfo a;
+	WFDB_Siginfo *siarray;
+	WFDB_Annotation annot;
+	unsigned long currentSample;
+    WFDB_Frequency recordSamplingFreq;
+    float freqScale;
+    WFDB_Time scaledTime;
 
 	if (argc < 2) {
 		strcpy(record, "100s");
@@ -24,20 +34,74 @@ int main(int argc, char *argv[])
 		strcpy(record, argv[1]);
 	}
 
-	cout << "Printing signal record : " << record << endl;
+	nsig = isigopen(record, NULL, 0);
+	if (nsig < 1)
+	    exit(1);
+	siarray = (WFDB_Siginfo *)malloc(nsig * sizeof(WFDB_Siginfo));
+	nsig = isigopen(record, siarray, nsig);
 
-	if (isigopen(record, s, 2) < 2)
-	{
-		cout << "Could not read record : " << record << endl;
+	/* Open the annotation "atr" in the annotation file */
+	a.name = "atr";
+	a.stat = WFDB_READ;
+	if (annopen(record, &a, 1) < 0)
+		cout << "Error opening annotation" << endl;
+
+	/* Get Records Sampling Frequency */
+	recordSamplingFreq = sampfreq(record );
+
+	/* Set System Sampling Frequency */
+	setifreq(SAPIEN_SAMPLING_FREQ);
+
+	/*
+	 * Frequency Scaling factor
+	 * Required for aligning annotations
+	 * with samples
+	 */
+	freqScale = SAPIEN_SAMPLING_FREQ / recordSamplingFreq;
+
+	/* Get annotation 0 (numbered from zero) */
+	while (getann(0, &annot) == 0) {
+		scaledTime = freqScale*annot.time;
+	    printf("sample no:%i (scaled:%i) time:%s annotation:%s\n", annot.time, scaledTime, mstimstr(annot.time), annstr(annot.anntyp));
 	}
-	else
-	{
-		while  (getvec(v) > 0) {
-			printf("%d\t%d\n", v[0], v[1]);
+
+	for (i = 0; i < nsig; i++)
+	    printf("signal %d gain = %g\n", i, siarray[i].gain);
+
+	/* Read the first annotation to get its sample no */
+	getann(0, &annot);
+	scaledTime = freqScale*annot.time;
+	currentSample=0;
+	/*
+	 * Run through all the record samples
+	 * getvec reads a sample and increments
+	 * the sample pointer
+	 */
+	while  (getvec(v) > 0  && currentSample<10)
+		{
+		printf("sample %06i val1:%04d val2:%04d", currentSample, v[0], v[1]);
+		/*
+		 * If the current sample has a
+		 * corresponding annotation,
+		 * add this to the string
+		 */
+		if (scaledTime == currentSample) {
+//		if (freqScale*annot.time == currentSample) {
+			printf(" %s\n", annstr(annot.anntyp));
+			if (annot.anntyp == NORMAL)
+				printf("BEAT\n");
+			if(getann(0, &annot) < 0)
+				exit(1);
+			scaledTime = freqScale*annot.time;
+			}
+		else
+			printf("\n");
+		currentSample++;
 		}
-	}
 
-    wfdbquit();
+	free(siarray);
+
+	wfdbquit();
 
 	return 0;
 }
